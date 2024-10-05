55d8647ed4556218f204936ac0d6f4a2cc2b14b4
Lada <lada11lada@gmail.com>
2024-10-05 10:10:11 +0200

    hw 2: corrections - introduced an id to customer table

--- scripts/transform_to_3nf.sql
+++ scripts/transform_to_3nf.sql
@@ -2,16 +2,17 @@ USE hw_2;
 
 -- removing the transitive dependency between customer name and customer address by creating a new table for customers
 CREATE TABLE IF NOT EXISTS customers_3NF (
-    customer_name VARCHAR(100) PRIMARY KEY,
-    customer_address VARCHAR(255)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    address VARCHAR(255)
 );
 
 -- new order table that references a customer table
 CREATE TABLE IF NOT EXISTS orders_3NF (
-    order_number INT PRIMARY KEY,
-    order_date DATE,
-    customer_name VARCHAR(100),
-    FOREIGN KEY (customer_name) REFERENCES customers_3NF(customer_name)
+    number INT PRIMARY KEY,
+    date DATE,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers_3NF(id)
 );
 
 -- new order details table with a composite key of order number and product name referencing orders table
@@ -20,21 +21,21 @@ CREATE TABLE IF NOT EXISTS order_details_3NF (
     product_name VARCHAR(100),
     product_quantity INT,
     PRIMARY KEY (order_number, product_name),
-    FOREIGN KEY (order_number) REFERENCES orders_3NF(order_number)
+    FOREIGN KEY (order_number) REFERENCES orders_3NF(number)
 );
 
 -- inserting existing data into tables ignoring the data that breaks the unique constraints
-INSERT IGNORE INTO customers_3NF (customer_name, customer_address)
+INSERT IGNORE INTO customers_3NF (name, address)
 VALUES
     ('Мельник', 'Хрещатик 1'),
     ('Шевченко', 'Басейна 2'),
     ('Коваленко', 'Комп’ютерна 3');
 
-INSERT IGNORE INTO orders_3NF (order_number, order_date, customer_name)
+INSERT IGNORE INTO orders_3NF (number, date, customer_id)
 VALUES
-    (101, '2023-03-15', 'Мельник'),
-    (102, '2023-03-16', 'Шевченко'),
-    (103, '2023-03-17', 'Коваленко');
+    (101, '2023-03-15', 1),
+    (102, '2023-03-16', 2),
+    (103, '2023-03-17', 3);
 
 INSERT IGNORE INTO order_details_3NF (order_number, product_name, product_quantity)
 VALUES
