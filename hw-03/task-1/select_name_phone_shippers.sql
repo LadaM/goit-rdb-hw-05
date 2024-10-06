289e4e7f64bdea6a0bbb350ffaf4313bac887b42
Lada <lada11lada@gmail.com>
2024-10-06 14:19:39 +0200

    hw-03: task 1

---
+++ hw-03/task-1/select_name_phone_shippers.sql
@@ -0,0 +1,4 @@
+use topic_3;
+
+select name, phone
+from shippers;
---
+++ hw-03/task-1/select_products.sql
@@ -0,0 +1,3 @@
+use topic_3; -- replace schema name if needed
+-- selecting all columns from the table products using wildcard
+select * from products;
